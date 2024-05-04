c3899aafee0da3dd7efb8ef65dec14dc842105f8
JelloHuang <ruifeihuang@gmail.com>
2024-05-04 12:25:18 +0800

    add setup outline server

---
+++ setup_outline.sh
@@ -0,0 +1,4 @@
+curl -sS https://get.docker.com/ | sh
+systemctl start docker
+systemctl enable docker
+wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash
