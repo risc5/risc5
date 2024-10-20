b53ec5de846af8bc6d19227a61f0eb1d0827eece
JelloHuang <ruifeihuang@gmail.com>
2024-10-20 09:32:37 +0800

    add remove docker

--- setup_outline.sh
+++ setup_outline.sh
@@ -2,3 +2,5 @@ curl -sS https://get.docker.com/ | sh
 systemctl start docker
 systemctl enable docker
 wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash
+#apt-get purge docker-ce
+#rm -rf /var/lib/docker 
